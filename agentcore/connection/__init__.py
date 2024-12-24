267d9d5be042e7f8d93faa7706f8806c9914ccf4
Jeroen van der Heijden <jeroen@cesbit.com>
2024-12-24 17:26:07 +0100

    rapp

--- agentcore/connection/__init__.py
+++ agentcore/connection/__init__.py
@@ -5,11 +5,21 @@ from .probeserverprotocol import ProbeServerProtocol
 
 
 PROBE_SERVER_PORT = int(os.getenv('PROBE_SERVER_PORT', 8750))
+RAPP_PORT = int(os.getenv('RAPP_PORT', 8770))
 
 
 def init_probe_server(loop: asyncio.AbstractEventLoop) -> \
         asyncio.AbstractServer:
     host, port = None, PROBE_SERVER_PORT
+
+    coro = loop.create_server(ProbeServerProtocol, host=host, port=port)
+    binding = '*' if host is None else host
+    logging.info(f'listening for probes on {binding}:{port}')
+    return loop.run_until_complete(coro)
+
+
+def init_rapp(loop: asyncio.AbstractEventLoop) -> asyncio.AbstractServer:
+    host, port = None, RAPP_PORT
 
     coro = loop.create_server(ProbeServerProtocol, host=host, port=port)
     binding = '*' if host is None else host
